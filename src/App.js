bdd5d920d0f2a752cfbf4cc49a7fea337f0f6549
nardo88 <nardo1988@mail.ru>
2021-11-24 16:45:56 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.scss';
+import PaginationComponent from './components/pagination';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="App-header">
+        <PaginationComponent />
+      </div>
     </div>
   );
 }
